555c134c8f377dfcbd5f19dd5a241bfbac7540a8
tjals <tjals@DESKTOP-C1RV364>
2020-12-02 19:52:27 +0900

    ㅇ

--- src/loginFeature/SignUp.java
+++ src/loginFeature/SignUp.java
@@ -1,54 +1,50 @@
 package loginFeature;
 
 import java.awt.BorderLayout;
+import java.awt.Button;
 import java.awt.CardLayout;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.LayoutManager;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.ArrayList;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class SignUp extends JFrame implements MouseListener {
+public class SignUp extends JFrame{
 
 	SignUpEnum sign;
+	final int GRID = 3;
+	JPanel grid_panel = new JPanel(new GridLayout(GRID, 1, 0, 5));
 
 	public SignUp() {
-		// sign.value.text;
-		// sign.value.label;
-
-		int view = 0;
+		
 		for (SignUpEnum value : SignUpEnum.values()) {
-			JLabel label = value.label;
+//			JLabel label = value.label;
+//			grid_panel.add(label);
 			JTextField text = value.text;
-
-			label.setBounds(0, view, 500, 50);
-			view += 50;
-			text.setBounds(0, view, 500, 50);
-			view += 50;
-			add(text);
-			add(label);
+			grid_panel.add(text);
 		}
 		
 		SwingTools.initTestFrame(this);
-		
-//		name = new JTextField();
-//		name.setBounds(50, 50, 300, 50);
-//		phoneNumber = new JTextField();
-//		phoneNumber.setBounds(50,  150, 300, 50);
-//		password = new JTextField();
-//		password.setBounds(50,  250, 300, 50);
-//		
-//
-//		add(name);
-//
-//		add(phoneNumber);
-//		
-//		add(password);
+		setLayout(new GridLayout(3, 3, 0, 0));
+		add(new JPanel());
+		add(new JPanel());
+		add(new JPanel());
+		add(new JPanel());
+		add(grid_panel);
+		add(new JPanel());
+		add(new JPanel());
+		add(new JPanel());
+		add(new JPanel());
 		
 	}
 
@@ -56,37 +52,11 @@ public class SignUp extends JFrame implements MouseListener {
 		new SignUp();
 	}
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if (e.getX() > 150 && e.getX() < 300 && e.getY() > 0 && e.getY() < 30) {
-//	            name.setText("");
-			repaint();
-		}
-
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
+//	@Override
+//	public void mouseClicked(MouseEvent e) {
+//		if (e.getX() > 150 && e.getX() < 300 && e.getY() > 0 && e.getY() < 30) {
+////	            name.setText("");
+//			repaint();
+//		}
+//	}
 }
