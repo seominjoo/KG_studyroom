14b4e9e28c3a7b9004d976992091b5eff259caa1
YJH <YJH@LAPTOP-S7MIH2JN>
2020-12-05 15:30:51 +0900

    sql

---
+++ sql/Seat_Price.sql
@@ -0,0 +1,8 @@
+INSERT INTO Seat_Price VALUES ('Daily_2Hr', 3000);
+INSERT INTO Seat_Price VALUES ('Daily_4Hr', 4500);
+INSERT INTO Seat_Price VALUES ('Daily_6Hr', 6000);
+INSERT INTO Seat_Price VALUES ('Daily_8Hr', 7500);
+INSERT INTO Seat_Price VALUES ('Daily_12Hr', 10000);
+INSERT INTO Seat_Price VALUES ('Weekly_2Wk', 90000);
+INSERT INTO Seat_Price VALUES ('Weekly_4Wk', 130000);
+INSERT INTO Seat_Price VALUES ('Weekly_8Wk', 250000);
