fd8f4de559849dd7f687995e2fac08fa63740b83
Hyun <Hyun@DESKTOP-G219KAQ>
2020-12-07 02:43:43 +0900

    와 이걸.. 너가..

--- src/loginFeature/ClickSignUp.java
+++ src/loginFeature/ClickSignUp.java
@@ -37,6 +37,7 @@ public class ClickSignUp extends MouseAdapter {
 	static String pw;
 	static String pwConfirm;
 	static String phoneNumber;
+
 	boolean consentFlag = true;
 //
 //	Container card_panel;
@@ -47,49 +48,48 @@ public class ClickSignUp extends MouseAdapter {
 	static String year;
 	static String month;
 	static String day;
-	
-	
+
 	public ClickSignUp(Object year, Object month, Object day) {
 		this.year = year.toString();
 		this.month = month.toString();
 		this.day = day.toString();
 	}
-	
+
 	public ClickSignUp() {
-		
+
 	}
+	// ´ëÂî··~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
+		boolean samePhoneNumber = false;
+		String text = PhoneNumberEnum.PHONENUMBER1.text.getText() + "-" + PhoneNumberEnum.PHONENUMBER2.text.getText()
+				+ "-" + PhoneNumberEnum.PHONENUMBER3.text.getText();
 
 		pw = SignUpEnum.PASSWORD.blindPW.getText();
 		pwConfirm = SignUpEnum.PASSWORDCONFIRM.blindPW.getText();
 
 		if (e.getButton() == MouseEvent.BUTTON1) {
-			if(!Pattern.matches("[°¡-ÆR]{2,4}", 
-					SignUpEnum.NAME.text.getText()))
+			if (!Pattern.matches("[°¡-ÆR]{2,4}", SignUpEnum.NAME.text.getText()))
 				new NameFailWindow();
-			else if(!Pattern.matches("[0-9]{6,8}", year+month+day)) {
+			else if (!Pattern.matches("[0-9]{6,8}", year + month + day)) {
 				new BirthFailWindow();
-			}
-			else if(!(Pattern.matches("01[0-9]", PhoneNumberEnum.PHONENUMBER1.text.getText())
+			} else if (!(Pattern.matches("01[0-9]", PhoneNumberEnum.PHONENUMBER1.text.getText())
 					&& Pattern.matches("[0-9]{4}", PhoneNumberEnum.PHONENUMBER2.text.getText())
-							&& Pattern.matches("[0-9]{4}", PhoneNumberEnum.PHONENUMBER3.text.getText())))
-					new PhoneNumberFailWindow();
-			else if(!Pattern.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,12}$", 
-					pw))
+					&& Pattern.matches("[0-9]{4}", PhoneNumberEnum.PHONENUMBER3.text.getText())))
+				new PhoneNumberFailWindow();
+			else if (!Pattern.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z]).{8,12}$", pw))
 				new PassWordFailWindow();
 			else if (!pw.equals(pwConfirm) || pw.equals(SignUpEnum.PASSWORDCONFIRM.labelName))
 				new PWnoMatch();
-			 else {
+			else {
 				// ¾à°ü Ã¼Å©
 				for (Entry<JCheckBox, JButton> kv : SignUp.consent.entrySet()) {
 					if (!kv.getKey().isSelected()) {
 						new ConsentFailWindow();
 						consentFlag = false;
 						break;
-					}
-					else
+					} else
 						consentFlag = true;
 				}
 
@@ -100,82 +100,81 @@ public class ClickSignUp extends MouseAdapter {
 						// ÀÌ¸§, »ý³â¿ùÀÏ, ÈÞ´ëÆù, ºñ¹ø,
 
 						Class.forName("oracle.jdbc.driver.OracleDriver");
-						System.out.println("[ojdbc] ¼º°øÀûÀ¸·Î ·ÎµåµÇ¾ú½À´Ï´Ù.");
+
 						Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521/XEPDB1", "hr",
 								"1234");
-						System.out.println("¿¬°á ¼º°ø");
-						
+
 						conn.setAutoCommit(false);
-						
-//						PreparedStatement read_PhoneNumber = conn
-//								.prepareStatement("SELECT phone_number FROM person_info");
-//
-//						ResultSet rs = read_PhoneNumber.executeQuery();
-//
-//						while (rs.next()) {
-//							phoneNumber = rs.getString("phone_number");
-//							System.out.println(phoneNumber);
-//						}
-//						System.out.println(PhoneNumberEnum.PHONENUMBER1.text.getText()+"-"
-//								+PhoneNumberEnum.PHONENUMBER2.text.getText()+"-"
-//								+PhoneNumberEnum.PHONENUMBER3.text.getText());
-////						if(phoneNumber.equals() {
-////							new SamePhoneNumberFail();
-////						}
-//							
-//						if(rs != null) rs.close();
-//						if(read_PhoneNumber != null) read_PhoneNumber.close();
-						
-						PreparedStatement insertPersonInfo = conn.prepareStatement("INSERT INTO Person_Info "
-								+ "(Person_Id,Check_Time,Person_Name, person_birth, Phone_Number,PW,Total_Payment)"
-								+ " VALUES(SignUpSeq.nextval, ?, ?, ?, ?, ?, ?)");
+						//
+						PreparedStatement read_PhoneNumber = conn
+								.prepareStatement("SELECT phone_number FROM person_info");
 
-						// Batch : ÀÏ°ýÃ³¸®
-						DateFormat simple = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-						Date now = new Date();
+						ResultSet rs = read_PhoneNumber.executeQuery();
 
-						insertPersonInfo.setString(1, simple.format(now));
-						insertPersonInfo.setString(2, SignUpEnum.NAME.text.getText());
-						insertPersonInfo.setString(3, year+month+day);
-						insertPersonInfo.setString(4, PhoneNumberEnum.PHONENUMBER1.text.getText()+"-"
-								+PhoneNumberEnum.PHONENUMBER2.text.getText()+"-"
-								+PhoneNumberEnum.PHONENUMBER3.text.getText());
-						insertPersonInfo.setString(5, pw);
-						insertPersonInfo.setInt(6, 0);
-						insertPersonInfo.addBatch();
+						while (rs.next()) {
+							phoneNumber = rs.getString("phone_number");
+							if (phoneNumber.equals(text)) {
+								new SamePhoneNumberFail();
 
-						int[] rows = insertPersonInfo.executeBatch();
-
-						if (insertPersonInfo != null)
-							insertPersonInfo.close();
-
-						PreparedStatement read_name_ID_from_personInfo = conn
-								.prepareStatement("SELECT person_id, person_name FROM person_info");
-
-						ResultSet rs2 = read_name_ID_from_personInfo.executeQuery();
-
-						while (rs2.next()) {
-
-							person_id = rs2.getInt(1);
-							person_name = rs2.getString(2);
+								samePhoneNumber = true;
+								break;
+							}
 						}
 
-						new SignUpSuccessWindow(person_id, person_name);
+						if (rs != null)
+							rs.close();
+						if (read_PhoneNumber != null)
+							read_PhoneNumber.close();
 
-						if (rs2 != null)
-							rs2.close();
-						if (read_name_ID_from_personInfo != null)
-							read_name_ID_from_personInfo.close();
-						if (conn != null)
-							conn.close();
+						if (!samePhoneNumber) {
+							PreparedStatement insertPersonInfo = conn.prepareStatement("INSERT INTO Person_Info "
+									+ "(Person_Id,Check_Time,Person_Name, person_birth, Phone_Number,PW,Total_Payment)"
+									+ " VALUES(SignUpSeq.nextval, ?, ?, ?, ?, ?, ?)");
 
-						System.out.println("¼º°ø");
+							// Batch : ÀÏ°ýÃ³¸®
+							DateFormat simple = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+							Date now = new Date();
+
+							insertPersonInfo.setString(1, simple.format(now));
+							insertPersonInfo.setString(2, SignUpEnum.NAME.text.getText());
+							insertPersonInfo.setString(3, year + month + day);
+							insertPersonInfo.setString(4, text);
+							insertPersonInfo.setString(5, pw);
+							insertPersonInfo.setInt(6, 0);
+							insertPersonInfo.addBatch();
+
+							int[] rows = insertPersonInfo.executeBatch();
+
+							if (insertPersonInfo != null)
+								insertPersonInfo.close();
+
+							PreparedStatement read_name_ID_from_personInfo = conn.prepareStatement(
+									"SELECT person_id, person_name FROM person_info order by check_time");
+
+							ResultSet rs2 = read_name_ID_from_personInfo.executeQuery();
+
+							while (rs2.next()) {
+								person_id = rs2.getInt(1);
+								person_name = rs2.getString(2);
+							}
+
+							new SignUpSuccessWindow(person_id, person_name);
+
+							if (rs2 != null)
+								rs2.close();
+							if (read_name_ID_from_personInfo != null)
+								read_name_ID_from_personInfo.close();
+							if (conn != null)
+								conn.close();
+
+							System.out.println("¼º°ø");
+						}
 
 					} catch (SQLException e1) {
-						 //e1.printStackTrace();
+						// e1.printStackTrace();
 						System.out.println(e1.toString());
-						//new SignUpFailWindow(e1);
-	
+						// new SignUpFailWindow(e1);
+
 					} catch (ClassNotFoundException e1) {
 						e1.printStackTrace();
 						System.out.println("[ojdbc] Å¬·¡½º °æ·Î°¡ Æ²·È½À´Ï´Ù.");
--- src/loginFeature/SignUpEnum.java
+++ src/loginFeature/SignUpEnum.java
@@ -8,11 +8,11 @@ import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public enum SignUpEnum {
-	NAME("Name *","성          함             "),
-	BIRTHDAY("Birthday *", "생 년 월 일              "),
-	PHONENUMBER("Phone Number *", "전 화 번 호             "),
-	PASSWORD("Password", "비 밀 번 호             "),
-	PASSWORDCONFIRM("Password", "비 밀 번 호  확 인      ");
+	NAME("Name *","성          함             ", ""),
+	BIRTHDAY("Birthday *", "생 년 월 일              ", ""),
+	PHONENUMBER("Phone Number *", "전 화 번 호             ", ""),
+	PASSWORD("Password", "비 밀 번 호             ", "12341234Aa!"),
+	PASSWORDCONFIRM("Password", "비 밀 번 호  확 인      ", "12341234Aa!");
 	
 	
 	JLabel label;
@@ -21,10 +21,10 @@ public enum SignUpEnum {
 	String labelName;
 	String labelNameKor;
 	
-	SignUpEnum(String labelName, String labelNameKor) {
+	SignUpEnum(String labelName, String labelNameKor, String string) {
 		this.label = new JLabel(labelName, label.CENTER);
 		this.text = new JTextField(labelName, text.CENTER);
-		this.blindPW = new JPasswordField(labelName);
+		this.blindPW = new JPasswordField(string);
 		this.labelName = labelName;
 		this.labelNameKor = labelNameKor;
 	}
