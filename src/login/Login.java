3ac6233ad5f843fca156a09b95f94c2fd752342b
tjals <tjals@DESKTOP-4J4UDHL>
2020-12-06 14:44:56 +0900

    12/6 2:44 커밋

--- src/login/Login.java
+++ src/login/Login.java
@@ -30,75 +30,51 @@ public class Login extends JFrame {
 		background.setBounds(0, 0, 624, 373);
 	
 		JTextField phone_number1 = new JTextField("010");
-		phone_number1.setOpaque(false); // 배경 투명
-		phone_number1.setBorder(BorderFactory.createLineBorder(Color.decode("#cfab8b"))); // 테두리?
-		phone_number1.setHorizontalAlignment(SwingConstants.CENTER);
-		phone_number1.setForeground(Color.decode("#cfab8b"));
+		new style(phone_number1);
 		phone_number1.setBounds(340, 137, 40, 30);
 		background.add(phone_number1);
 		
-		JLabel str = new JLabel("-", JLabel.CENTER);
-		str.setBounds(380, 137, 10, 30);
-		str.setForeground(Color.decode("#cfab8b"));
-		background.add(str);
+		JLabel hyphen = new JLabel("-", JLabel.CENTER);
+		new style(hyphen);
+		hyphen.setBounds(380, 137, 10, 30);
+		background.add(hyphen);
 		
 		JTextField phone_number2 = new JTextField();
-		phone_number2.setOpaque(false); // 배경 투명
-		phone_number2.setBorder(BorderFactory.createLineBorder(Color.decode("#cfab8b"))); // 테두리?
-		phone_number2.setHorizontalAlignment(SwingConstants.CENTER);
-		phone_number2.setForeground(Color.decode("#cfab8b"));
+		new style(phone_number2);
 		phone_number2.setBounds(390, 137, 45, 30);
 		background.add(phone_number2);
 		
-		JLabel str2 = new JLabel("-", JLabel.CENTER);
-		str2.setBounds(435, 137, 10, 30);
-		str2.setForeground(Color.decode("#cfab8b"));
-		background.add(str2);
+		JLabel hyphen2 = new JLabel("-", JLabel.CENTER);
+		new style(hyphen2);
+		hyphen2.setBounds(435, 137, 10, 30);
+		background.add(hyphen2);
 		
 		JTextField phone_number3 = new JTextField();
-		phone_number3.setOpaque(false); // 배경 투명
-		phone_number3.setBorder(BorderFactory.createLineBorder(Color.decode("#cfab8b"))); // 테두리?
-		phone_number3.setHorizontalAlignment(SwingConstants.CENTER);
-		phone_number3.setForeground(Color.decode("#cfab8b"));
+		new style(phone_number3);
 		phone_number3.setBounds(445, 137, 45, 30);
 		background.add(phone_number3);
 		
 		JPasswordField logpass = new JPasswordField("비밀번호");
-		logpass.setOpaque(false); // 배경 투명
-		logpass.setBorder(BorderFactory.createLineBorder(Color.decode("#cfab8b"))); // 테두리?
-		logpass.setHorizontalAlignment(SwingConstants.CENTER);
-		logpass.setForeground(Color.decode("#cfab8b"));
+		new style(logpass);
 		logpass.setBounds(340, 177, 150, 30);
 		background.add(logpass);
 		
 		JButton login = new JButton("로그인");
-		login.setBackground(Color.decode("#cfab8b"));
-		login.setFocusable(false);
-		login.setOpaque(false); // 배경 투명
-		login.setFocusPainted(false);
-		login.setBorder(BorderFactory.createLineBorder(Color.decode("#cfab8b"))); // 테두리?
-		login.setForeground(Color.decode("#cfab8b"));
+		new style(login);
 		login.setBounds(340, 217, 150, 30);
 		login.addActionListener(new Notification(login));
-		
 		background.add(login);
 		
-		
-		
 		JButton find_PW = new JButton(new Conversion_image("image/PW찾기.png", 40, 40).imageicon_smooth);
-		find_PW.setForeground(Color.decode("#cfab8b"));
-		find_PW.setBorderPainted(false); // 외곽선을 없앰
-		find_PW.setContentAreaFilled(false); //내용영역을 안채움
-		find_PW.setFocusPainted(false); // 선택시 테두리 끔
+		new style(find_PW);
 		find_PW.setBounds(545, 247, 50, 50);
+		find_PW.addActionListener(new Notification(find_PW));
 		background.add(find_PW);
 	
 		JButton Signup = new JButton(new Conversion_image("image/회원가입.png", 40	, 40).imageicon_smooth);
-		Signup.setForeground(Color.decode("#cfab8b"));
-		Signup.setBorderPainted(false); // 외곽선을 없앰
-		Signup.setContentAreaFilled(false); //내용영역을 안채움
-		Signup.setFocusPainted(false); // 선택시 테두리 끔
+		new style(Signup);
 		Signup.setBounds(543, 292, 50, 50);
+		Signup.addActionListener(new Notification(Signup));
 		background.add(Signup);
 		
 		addMouseListener(new MouseAdapter() {
--- src/login/Notification.java
+++ src/login/Notification.java
@@ -3,6 +3,8 @@ package login;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -22,25 +24,33 @@ public class Notification extends JFrame implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 
-		SwingTool_logo.initFrame(this);
-		setLayout(new BorderLayout());
-		setBounds(250, 170, 300, 150);
-		JPanel laynull = new JPanel();
+		
+		JPanel laynull = new JPanel(new GridLayout(2, 0, 0, 0));
+		new style(laynull);
 		JLabel commenteng = new JLabel("[system] still in maintenance", JLabel.CENTER);
-		commenteng.setBounds(x, y, width, height);
+		new style(commenteng);
 		JLabel commentkor = new JLabel("페이지 준비 중..", JLabel.CENTER);
+		new style(commentkor);
+		
+		SwingTool_logo.initFrame(this);
+		getContentPane().setBackground(Color.decode("#404040"));
+		setLayout(new BorderLayout(10,10));
+		setBounds(250, 170, 300, 150);
 		
 		laynull.add(commenteng);
 		laynull.add(commentkor);
-		add(commentkor, BorderLayout.CENTER);
 		
 		JButton combtn = new JButton("닫기");
+		new style(combtn);
 		combtn.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				dispose();
 			}
 		});
+		
+		add(new style().getnewPanel(), BorderLayout.NORTH);
+		add(laynull, BorderLayout.CENTER);
 		add(combtn, BorderLayout.SOUTH);
 	}
 
