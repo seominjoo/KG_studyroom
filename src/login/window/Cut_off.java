3f5fd46653ad778566ffd376b02e293d166d9205
tjals <tjals@DESKTOP-4J4UDHL>
2020-12-07 23:08:09 +0900

    dy

---
+++ src/login/window/Cut_off.java
@@ -0,0 +1,20 @@
+package login.window;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JTextField;
+
+public class Cut_off extends KeyAdapter{
+	
+	
+	public Cut_off(JTextField restricted) {
+		
+	}
+	
+	@Override
+	public void keyPressed(KeyEvent e) {
+//		e.get
+	}
+}
