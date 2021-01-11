64c6825012cf072593dac08d443f5e569a9ec568
username <user@email.mail>
2021-01-12 00:31:25 +0900

    커밋

--- js/js11.js
+++ js/js11.js
@@ -1,5 +1,6 @@
 caltext1 = document.getElementById('ctext1');
 caltext2 = document.getElementById('ctext2');
+modal = document.getElementById("calhistory");
 
 check = 'true';
 result = 0;
@@ -7,6 +8,7 @@ result = 0;
 function AC() {
     caltext1.value = "";
     caltext2.value = '0';
+    modal.innerHTML = "";
 }
 
 function del() {
@@ -49,11 +51,12 @@ function calresult() {
     caltext1.value += caltext2.value;
     caltext2.value = calculate(caltext1.value);
     result = 1;
+    modal.innerHTML += `<p>${caltext1.value}<br>${caltext2.value}</p>`
 }
 
-function calculate(preview){
+function calculate(string){
 
-    splited = preview.split(/(?<=[*/+-])|(?=[*/+-])/); 
+    splited = string.split(/(?<=[*/%()+-])|(?=[*/%+()-])/); 
     splited2 = splited
 
     for ( i = 0; i < splited2.length; i++) {
@@ -74,7 +77,19 @@ function calculate(preview){
             splited2.splice(i+1, 1);
             splited2.splice(i, 1);
             i--;
-        } else if(splited2[i]==="+") {
+        }else if (splited2[i]==="%") {
+            temp_int = Number.parseInt(splited2[i - 1]) % Number.parseInt(splited2[i + 1]);
+            temp = temp_int + "";
+            splited2[i - 1] = temp;
+            splited2.splice(i+1, 1);
+            splited2.splice(i, 1);
+            i--;
+        }
+    }
+    for ( i = 0; i < splited2.length; i++) {
+            temp = "";
+            temp_int = 0;
+    if(splited2[i]==="+") {
             temp_int = Number.parseInt(splited2[i - 1]) + Number.parseInt(splited2[i + 1]);
             temp = temp_int + "";
             splited2[i - 1]= temp;
@@ -88,8 +103,15 @@ function calculate(preview){
             splited2.splice(i+1, 1);
             splited2.splice(i, 1);
             i--;
-        }else{
         }
     }
     return splited2[0];
+}
+
+function caldisplay() {
+    if( modal.style.display ==='none'){
+        modal.style.display = "block";
+    }else{
+        modal.style.display="none";
+    }
 }
